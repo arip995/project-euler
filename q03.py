a46bef8ca4443eedf28346c1e718a8318543f60d
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-07 22:52:35 +0530

    optimised

--- q03.py
+++ q03.py
@@ -1,11 +1,8 @@
-import math
-
-factors = []
 n = 600851475143
+i = 2
+while i * i < n:
+     while n % i == 0:
+         n = n / i
+     i = i + 1
 
-for i in range(2, math.ceil(math.sqrt(n))):
-    while n % i == 0:
-        factors.append(i)
-        n = n / i
-
-print(factors[-1])
+print (n)
