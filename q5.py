4712b46cf180e3fd45315290c6d3be38879f1c81
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-07 22:57:21 +0530

    used formula

--- q5.py
+++ q5.py
@@ -1,10 +1,8 @@
-i = 20;
- 
-while (i %  2 != 0 or i %  3 != 0 or i %  4 != 0 or i %  5 != 0 or
-         i %  6 != 0 or i %  7 != 0 or i %  8 != 0 or i %  9 != 0 or
-         i % 10 != 0 or i % 11 != 0 or i % 12 != 0 or i % 13 != 0 or
-         i % 14 != 0 or i % 15 != 0 or i % 16 != 0 or i % 17 != 0 or
-         i % 18 != 0 or i % 19 != 0 or i % 20 != 0):
-    i += 20;
-
-print(i)
+import fractions
+def lcm(n): 
+    ans = 1    
+    for i in range(1, n + 1): 
+        ans = (ans * i)/fractions.gcd(ans, i)         
+    print(ans) 
+x= 20
+lcm(x)
