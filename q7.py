af91608a9219e0211ec134f50b52f23e1bfd2562
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-07 22:55:18 +0530

    even number not checked

--- q7.py
+++ q7.py
@@ -1,17 +1,18 @@
-th=0
-i=2
+th=1
+i=3
 while True:
-    c=1
-    k=int(i/2+1)
-    for j in range(1,k):
-        if i%j==0:
-            c+=1
-    
-    if c==2:
-        th+=1
-    if th==10001:
-        print(i)
-        break
-    i+=1    
+    if i!=0:
+        c=1
+        k=int(i/2+1)
+        for j in range(1,k):
+            if i%j==0:
+                c+=1
+        
+        if c==2:
+            th+=1
+        if th==10001:
+            print(i)
+            break
+        i+=2   
 
 
