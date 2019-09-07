e797f9c735c1d3301252b819892a3dd0612ce406
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-07 22:56:41 +0530

    used formula

--- q6.py
+++ q6.py
@@ -1,7 +1,10 @@
-x=0
-z=0
-for i in range(1,101):
-    z=(i**2)+z
-    x+=i
-diff=(x**2)-z
-print(diff)
+sguare=0
+sum=0
+result=0
+n=101
+for i in range(1,n):
+    sum = n * (n+1)/ 2
+    square = (n * (n + 1) * (2 * n + 1)) / 6
+    
+    result = sum * sum - square
+print(result)
