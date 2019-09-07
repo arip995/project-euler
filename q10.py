a1f9c751648b069f7c34584e6c4a9df0492ca222
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-07 22:54:19 +0530

    even number not checked

--- q10.py
+++ q10.py
@@ -1,11 +1,12 @@
 
 sum = 0
-for i in range(2, 2000000):
-    c=1
-    for j in range(2, int(i/2) + 1):
-        if i % j == 0:
-            c+=1
-            if c==2:
-                sum += i
+for i in range(2,2000000):
+    if i!=0:
+        c=1
+        for j in range(2, int(i/2) + 1):
+            if i % j == 0:
+                c+=1
+                if c==2:
+                    sum += i
 
 print (sum)
