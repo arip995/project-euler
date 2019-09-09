a683f77309fc7efe4e970d33ec19fcba97b73eda
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-09 23:54:58 +0530

    used formula

--- q6.py
+++ q6.py
@@ -2,9 +2,7 @@ sguare=0
 sum=0
 result=0
 n=101
-for i in range(1,n):
-    sum = n * (n+1)/ 2
-    square = (n * (n + 1) * (2 * n + 1)) / 6
-    
-    result = sum * sum - square
+sum = n * (n+1)/ 2
+square = (n * (n + 1) * (2 * n + 1)) / 6
+result = sum * sum - square
 print(result)
