7cd0b6799a64351281bd08c742b12f1ebaf1b3b7
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-09 23:54:04 +0530

    optimised loop

--- q10.py
+++ q10.py
@@ -1,12 +1,13 @@
 
 sum = 0
 for i in range(2,2000000):
-    if i!=0:
-        c=1
+    c=1
+    if i/2!=0:
         for j in range(2, int(i/2) + 1):
-            if i % j == 0:
-                c+=1
-                if c==2:
-                    sum += i
+            if j/2!=0:
+                if i % j == 0:
+                    c+=1
+                    if c==2:
+                        sum += i
 
 print (sum)
