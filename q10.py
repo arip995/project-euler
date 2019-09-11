3b4c599749e69d7627d78c806e86750dc6acabc9
arip995 <52366427+arip995@users.noreply.github.com>
2019-09-11 08:00:36 +0530

    optimised

--- q10.py
+++ q10.py
@@ -1,13 +1,12 @@
+def isPrime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 sum = 0
-for i in range(2,2000000):
-    c=1
-    if i/2!=0:
-        for j in range(2, int(i/2) + 1):
-            if j/2!=0:
-                if i % j == 0:
-                    c+=1
-                    if c==2:
-                        sum += i
+for i in range(2, 2000000):
+    if isPrime(i):
+        sum += i
 
 print (sum)
